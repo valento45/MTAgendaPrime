197ebae935b2e66d9d1f2a510aaf946bc0457594
igor helio <iggor1935@hotmail.com>
2020-09-19 16:49:25 -0300

    TDD

--- AgendaTDD/ClienteTDD.cs
+++ AgendaTDD/ClienteTDD.cs
@@ -12,6 +12,29 @@ namespace AgendaTDD
     [TestFixture]
     public class ClienteTDD
     {
-       
+        string value;
+        
+       //SetUp defina o local onde será executado antes do teste propriamente.
+       //Muito útil para organizar dados necessários
+       [SetUp]
+        public void SetUp()
+        {
+             value = "123";            
+        }
+
+        [Test]
+        public void VerificaSeTemNumeros()
+        {
+            bool capturou = false;
+            for (int i = 0; i < value.Length; i++)
+                if (char.IsNumber(value[i]))
+                    capturou = true;
+
+
+            Assert.IsTrue(capturou);
+        }
+
+
+
     }
 }
--- FuncoesAuxiliares/FuncoesAuxiliares.cs
+++ FuncoesAuxiliares/FuncoesAuxiliares.cs
@@ -52,5 +52,18 @@ namespace FuncoesAuxiliares
             digito = digito + resto.ToString();
             return cpf.EndsWith(digito);
         }
+        /// <summary>
+        /// Verifica em uma cadeia de string se tem caracteres numerais.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool  VerificaSeTemNumero(string value)
+        {
+            bool capturou = false;
+            for (int i = 0; i < value.Length; i++)
+                if (char.IsNumber(value[i]))
+                    capturou = true;
+            return capturou;
+        }
     }
 }
